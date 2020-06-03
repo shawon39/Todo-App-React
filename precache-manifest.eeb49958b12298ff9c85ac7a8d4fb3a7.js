16fa6723472e571d728c767c378c38b7592b13e3
shawon39 <shshawon39@gmail.com>
2020-06-03 16:53:56 +0600

    Updates

---
+++ precache-manifest.eeb49958b12298ff9c85ac7a8d4fb3a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37530cdc9a461483821b60faff4a5d7",
+    "url": "/Todo-App-React/index.html"
+  },
+  {
+    "revision": "61114a161ce9211b5f98",
+    "url": "/Todo-App-React/static/css/main.08574d80.chunk.css"
+  },
+  {
+    "revision": "7e8dfdf7837bdb00f923",
+    "url": "/Todo-App-React/static/js/2.a80cdb28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-App-React/static/js/2.a80cdb28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61114a161ce9211b5f98",
+    "url": "/Todo-App-React/static/js/main.9c9a6910.chunk.js"
+  },
+  {
+    "revision": "0e857190be8b63768809",
+    "url": "/Todo-App-React/static/js/runtime-main.27872ddb.js"
+  }
+]);
